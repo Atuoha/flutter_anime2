d32c505152151ac63539c63959b11e119c3587c6
Atuoha <atuohainitiatives@gmail.com>
2022-12-10 17:14:05 +0100

    full impl of favorite btn anime

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -10,36 +10,56 @@ class Heart extends StatefulWidget {
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   AnimationController? _controller;
   Animation? _colorAnimation;
+  Animation? _sizeAnimation;
 
   @override
   void initState() {
     super.initState();
 
     _controller = AnimationController(
-      duration: const Duration(milliseconds: 200),
+      duration: const Duration(milliseconds: 1000),
       vsync: this,
     );
 
     _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red)
         .animate(_controller!);
 
-    _controller!.forward();
+    _sizeAnimation = TweenSequence([
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 30, end: 60), weight: 50),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 60, end: 30), weight: 50),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 30, end: 60), weight: 50),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 60, end: 30), weight: 50),
+    ]).animate(_controller!);
+  }
 
-    _controller!.addListener(() {
-      print(_controller!.value);
-      print(_colorAnimation!.value);
-    });
+  @override
+  void dispose() {
+    _controller!.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
+    return AnimatedBuilder(
+      animation: _controller!,
+      builder: (context, _) => IconButton(
+        icon: Icon(
+          Icons.favorite,
+          color: _colorAnimation!.value,
+          size: _sizeAnimation!.value,
+        ),
+        onPressed: () {
+          if (_controller!.isCompleted) {
+            _controller!.reset();
+          } else {
+            _controller!.forward();
+          }
+        },
       ),
-      onPressed: () {},
     );
   }
 }
